b82225f734a219a6102cacabe6fe94283f1618a0
Takashi Harano <takashiharano@users.noreply.github.com>
2020-11-07 00:11:33 +0900

    Update JsonBuilder

--- java/src/main/java/com/takashiharano/util/JsonBuilder.java
+++ java/src/main/java/com/takashiharano/util/JsonBuilder.java
@@ -8,18 +8,30 @@ public class JsonBuilder {
   StringBuilder buffer;
   int count;
   List<Integer> listCounter;
+  boolean isPartial;
 
   public JsonBuilder() {
+    this(false);
+  }
+
+  public JsonBuilder(boolean isPartial) {
     buffer = new StringBuilder();
     listCounter = new ArrayList<>();
-    buffer.append("{");
+    this.isPartial = isPartial;
+    if (!isPartial) {
+      buffer.append("{");
+    }
+  }
+
+  public void append(String value) {
+    buffer.append(value);
   }
 
   public void append(String key, String value) {
     if (value == null) {
       value = "null";
     } else {
-      value = StrUtil.quote(value);
+      value = _quoteAndEscape(value);
     }
     _append(key, value);
   }
@@ -59,7 +71,7 @@ public class JsonBuilder {
       if (i > 0) {
         sb.append(",");
       }
-      sb.append(StrUtil.quote(array[i]));
+      sb.append(_quoteAndEscape(array[i]));
     }
     sb.append("]");
     _append(key, sb.toString());
@@ -133,41 +145,90 @@ public class JsonBuilder {
     _append(key, sb.toString());
   }
 
+  public void appendComma() {
+    buffer.append(",");
+  }
+
+  public void openList() {
+    _newListCounter();
+    buffer.append("[");
+  }
+
   public void openList(String key) {
-    newListCounter();
+    _newListCounter();
     _append(key, "[");
   }
 
+  public void closeList() {
+    buffer.append("]");
+    _removeListCounter();
+  }
+
   public void appendListItem(String value) {
-    int listCount = getListCount();
+    if (value == null) {
+      value = "null";
+    } else {
+      value = _quoteAndEscape(value);
+    }
+    _appendListItem(value);
+  }
+
+  public void appendListItemJson(String json) {
+    _appendListItem(json);
+  }
+
+  public void appendListItem(int value) {
+    _appendListItem(Integer.toString(value));
+  }
+
+  public void appendListItem(long value) {
+    _appendListItem(Long.toString(value));
+  }
+
+  public void appendListItem(float value) {
+    _appendListItem(Float.toString(value));
+  }
+
+  public void appendListItem(double value) {
+    _appendListItem(Double.toString(value));
+  }
+
+  public void appendListItem(boolean value) {
+    _appendListItem(Boolean.toString(value));
+  }
+
+  public void _appendListItem(String value) {
+    int listCount = _getListCount();
     if (listCount > 0) {
       buffer.append(",");
     }
     buffer.append(value);
-    incrementListCounter();
-  }
-
-  public void closeList(String key) {
-    buffer.append("]");
-    removeListCounter();
+    _incrementListCounter();
   }
 
   private void _append(String key, String value) {
     if (count > 0) {
       buffer.append(",");
     }
-    buffer.append(StrUtil.quote(key));
+    buffer.append(_quoteAndEscape(key));
     buffer.append(":");
     buffer.append(value);
     count++;
   }
 
-  private void newListCounter() {
+  private String _quoteAndEscape(String value) {
+    value = value.replace("\\", "\\\\").replace("\r\n", "\\r\\n").replace("\n", "\\n").replace("\r", "\\r")
+        .replace("\t", "\\t");
+    value = StrUtil.quote(value);
+    return value;
+  }
+
+  private void _newListCounter() {
     Integer counter = 0;
     listCounter.add(counter);
   }
 
-  private int getListCount() {
+  private int _getListCount() {
     int listCount = 0;
     int size = listCounter.size();
     if (size > 0) {
@@ -176,16 +237,18 @@ public class JsonBuilder {
     return listCount;
   }
 
-  private void incrementListCounter() {
+  private void _incrementListCounter() {
     Integer counter = null;
     int size = listCounter.size();
     if (size > 0) {
-      counter = listCounter.get(size - 1);
+      int index = size - 1;
+      counter = listCounter.get(index);
       counter++;
+      listCounter.set(index, counter);
     }
   }
 
-  private void removeListCounter() {
+  private void _removeListCounter() {
     int size = listCounter.size();
     if (size > 0) {
       listCounter.remove(size - 1);
@@ -193,7 +256,12 @@ public class JsonBuilder {
   }
 
   public String toString() {
-    String json = buffer.toString() + "}";
+    String json;
+    if (isPartial) {
+      json = buffer.toString();
+    } else {
+      json = buffer.toString() + "}";
+    }
     return json;
   }
 
