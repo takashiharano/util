72e826c134cd32e189c401c2b8363dabda93d8db
Takashi Harano <takashiharano@users.noreply.github.com>
2021-01-09 12:57:01 +0900

    Add test code

---
+++ java/src/test/java/com/takashiharano/test/util/LogTTest.java
@@ -0,0 +1,39 @@
+package com.takashiharano.test.util;
+
+import com.takashiharano.util.Log;
+
+public class LogTTest {
+
+  public static void main(String args[]) {
+    test();
+  }
+
+  private static void test() {
+    Log.d("test start");
+
+    long t = Log.t("Start");
+
+    sleep(500);
+
+    t = Log.t("aaa1");
+    sleep(500);
+    t = Log.t("aaa2", t);
+
+    t = Log.t("bbb1");
+    sleep(1000);
+    t = Log.t("bbb2", t);
+
+    t = Log.t("ccc", t);
+
+    Log.d("test end");
+  }
+
+  private static void sleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
