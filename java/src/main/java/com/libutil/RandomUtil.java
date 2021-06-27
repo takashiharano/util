f7258dcc00881d7a8e9a956f538c7badc83ee51a
Takashi Harano <takashiharano@users.noreply.github.com>
2021-06-27 16:25:10 +0900

    Create RandomUtil.java (Move from Util.java)

---
+++ java/src/main/java/com/libutil/RandomUtil.java
@@ -0,0 +1,113 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2021 Takashi Harano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.libutil;
+
+import java.util.Random;
+
+public class RandomUtil {
+
+  /**
+   * Returns a random integer value.
+   *
+   * @return random integer value
+   */
+  public static int getInt() {
+    long seed = System.nanoTime();
+    Random r = new Random(seed);
+    return r.nextInt();
+  }
+
+  /**
+   * Returns a random integer value.
+   *
+   * @param max
+   * @return 0-max
+   */
+  public static int getInt(int max) {
+    long seed = System.nanoTime();
+    Random r = new Random(seed);
+    return r.nextInt(max + 1);
+  }
+
+  /**
+   * Returns a random integer value.
+   *
+   * @param min
+   * @param max
+   * @return min-max
+   */
+  public static int getInt(int min, int max) {
+    Random r = new Random(System.currentTimeMillis());
+    int i;
+    max++;
+    do {
+      i = r.nextInt(max);
+    } while (i < min);
+    return i;
+  }
+
+  /**
+   * Returns a random double value.<br>
+   * e.g.,) 0.8697886198087033
+   *
+   * @return 0.0 - 1.0
+   */
+  public static double getDouble() {
+    long seed = System.nanoTime();
+    Random r = new Random(seed);
+    return r.nextDouble();
+  }
+
+  public static boolean getBoolean() {
+    long seed = System.nanoTime();
+    Random r = new Random(seed);
+    return r.nextBoolean();
+  }
+
+  /**
+   * Returns a random string.<br>
+   * <br>
+   * randomString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+   * 8);<br>
+   * -> "TWEQWhq2"
+   * 
+   * @param table
+   * @param len
+   * @return
+   */
+  public static String getString(String table, int len) {
+    char[] cTable = table.toCharArray();
+    int lastIdx = cTable.length - 1;
+    char[] buf = new char[len];
+    long seed = System.nanoTime();
+    Random r = new Random(seed);
+    for (int i = 0; i < len; i++) {
+      int idx = r.nextInt(lastIdx + 1);
+      buf[i] = cTable[idx];
+    }
+    String ret = new String(buf);
+    return ret;
+  }
+
+}
