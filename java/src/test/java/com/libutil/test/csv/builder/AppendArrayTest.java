65827813b87bc93e206ceb27b84055b3bd6e6deb
Takashi Harano <takashiharano@users.noreply.github.com>
2021-08-08 01:55:28 +0900

    Update test code

--- java/src/test/java/com/libutil/test/csv/builder/AppendArrayTest.java
+++ java/src/test/java/com/libutil/test/csv/builder/AppendArrayTest.java
@@ -1,5 +1,8 @@
 package com.libutil.test.csv.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.libutil.CsvBuilder;
 import com.libutil.test.Log;
 
@@ -12,24 +15,43 @@ public class AppendArrayTest {
   private static void test() {
     CsvBuilder builder;
 
-    String chunk = "aa,bb,12";
-    builder = new CsvBuilder(",", true);
-    builder.append("abc");
-    builder.append(123);
-    builder.appendChunk(chunk);
+    String[] sArr = { "abc", "xyz" };
+    builder = new CsvBuilder();
+    builder.append(sArr);
     Log.i(builder);
 
-    builder = new CsvBuilder(",", true);
-    builder.appendChunk(chunk);
-    builder.append("abc");
-    builder.append(123);
+    List<String> sList = new ArrayList<>();
+    sList.add("abc");
+    sList.add("xyz");
+    builder = new CsvBuilder();
+    builder.append(sList);
     Log.i(builder);
 
-    builder = new CsvBuilder(",", true);
-    builder.append("abc");
-    builder.appendChunk(chunk);
-    builder.append(123);
+    int[] iArr = { 1, 2 };
+    builder = new CsvBuilder();
+    builder.append(iArr);
     Log.i(builder);
+
+    long[] lArr = { 1L, 2L };
+    builder = new CsvBuilder();
+    builder.append(lArr);
+    Log.i(builder);
+
+    float[] fArr = { 1.5f, 2.1f };
+    builder = new CsvBuilder();
+    builder.append(fArr);
+    Log.i(builder);
+
+    double[] dArr = { 1.5, 2.1 };
+    builder = new CsvBuilder();
+    builder.append(dArr);
+    Log.i(builder);
+
+    boolean[] bArr = { true, false };
+    builder = new CsvBuilder();
+    builder.append(bArr);
+    Log.i(builder);
+
   }
 
 }
