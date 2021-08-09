40fd0744d21a0c2ad3a9ced299be59ede5a7ad05
Takashi Harano <takashiharano@users.noreply.github.com>
2021-08-10 00:08:12 +0900

    Update test script for util.sh

--- sh/test/current_time.sh
+++ sh/test/current_time.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -u
-
 # shellcheck disable=SC1091
 # shellcheck disable=SC2034
-
 . ../util.sh
-
 trap error_handler ERR
 
-echo "----------------------------------------"
-echo "print_time test"
-echo "----------------------------------------"
-print_time
+current_time
+
 DATE_TIME_FORMAT="%Y-%m-%d %H:%M:%S.%3N %:z"
-print_time
-echo ""
+current_time
---
+++ sh/test/unixtime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -u
+# shellcheck disable=SC1091
+. ../util.sh
+trap error_handler ERR
+
+unixtime
