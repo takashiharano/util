c32214b71714ceaf8cfbe93b438cdf160a2ebdd9
Takashi Harano <takashiharano@users.noreply.github.com>
2021-09-08 00:49:24 +0900

    Rename RandomUtil to RandomGenerator

--- java/src/main/java/com/libutil/RandomGenerator.java
+++ java/src/main/java/com/libutil/RandomGenerator.java
@@ -28,14 +28,14 @@ import java.security.SecureRandom;
 /**
  * Generates random values.
  */
-public class RandomUtil {
+public class RandomGenerator {
 
   private static SecureRandom r = new SecureRandom();
 
   /**
    * Returns a random integer value.
    *
-   * @return random integer value
+   * @return a random integer value
    */
   public static int getInt() {
     return r.nextInt();
@@ -46,7 +46,7 @@ public class RandomUtil {
    *
    * @param max
    *          the max value
-   * @return 0-max
+   * @return a random integer value between 0 to max
    */
   public static int getInt(int max) {
     return r.nextInt(max + 1);
@@ -58,8 +58,8 @@ public class RandomUtil {
    * @param min
    *          the minimum value
    * @param max
-   *          the max value
-   * @return min-max
+   *          the maximum value
+   * @return a random integer value between min to max
    */
   public static int getInt(int min, int max) {
     int i;
@@ -71,10 +71,29 @@ public class RandomUtil {
   }
 
   /**
-   * Returns a random double value.<br>
-   * e.g.,) 0.8697886198087033
+   * Returns a random long value.
    *
-   * @return 0.0 - 1.0
+   * @return a random long value
+   */
+  public static long getLong() {
+    return r.nextLong();
+  }
+
+  /**
+   * Returns a random float value.<br>
+   * e.g., 0.017796695
+   *
+   * @return a random float value between 0.0 to 1.0
+   */
+  public static float getFloat() {
+    return r.nextFloat();
+  }
+
+  /**
+   * Returns a random double value.<br>
+   * e.g., 0.8697886198087033
+   *
+   * @return a random double value between 0.0 to 1.0
    */
   public static double getDouble() {
     return r.nextDouble();
@@ -90,20 +109,53 @@ public class RandomUtil {
   }
 
   /**
+   * Returns a random character value
+   *
+   * @return a random character value between 0 to 0xFFFF
+   */
+  public static char getChar() {
+    return (char) getInt(0, 0xFFFF);
+  }
+
+  /**
+   * Returns a random character value
+   *
+   * @param min
+   *          the minimum value
+   * @param max
+   *          the maximum value
+   * @return a random character value between min to max
+   */
+  public static char getChar(int min, int max) {
+    if (min > max) {
+      int w = min;
+      max = min;
+      min = w;
+    }
+    if (min < 0) {
+      min = 0;
+    }
+    if (max > 0xFFFF) {
+      max = 0xFFFF;
+    }
+    return (char) getInt(min, max);
+  }
+
+  /**
    * Returns a random string.<br>
    * <br>
    * getString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
    * 8);<br>
    * out: "TWEQWhq2"
    * 
-   * @param table
+   * @param chars
    *          characters to use
    * @param len
    *          length of the output
    * @return a randomly-generated string
    */
-  public static String getString(String table, int len) {
-    char[] cTable = table.toCharArray();
+  public static String getString(String chars, int len) {
+    char[] cTable = chars.toCharArray();
     int lastIdx = cTable.length - 1;
     char[] buf = new char[len];
     for (int i = 0; i < len; i++) {
