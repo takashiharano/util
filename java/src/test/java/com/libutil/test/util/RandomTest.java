55a377bdfc2cd91b9cca93386f9e0996d2f1d7c3
Takashi Harano <takashiharano@users.noreply.github.com>
2021-09-09 00:02:08 +0900

    Update RandomTest

--- java/src/test/java/com/libutil/test/util/RandomTest.java
+++ java/src/test/java/com/libutil/test/util/RandomTest.java
@@ -1,19 +1,27 @@
 package com.libutil.test.util;
 
-import com.libutil.RandomUtil;
+import com.libutil.RandomGenerator;
 import com.libutil.test.Log;
 
 public class RandomTest {
 
   public static void main(String args[]) {
-    Log.i("int       = " + RandomUtil.getInt());
-    Log.i("int 10    = " + RandomUtil.getInt(10));
-    Log.i("int 1-6   = " + RandomUtil.getInt(1, 6));
-    Log.i("int 10-20 = " + RandomUtil.getInt(10, 20));
-    Log.i("double    = " + RandomUtil.getDouble());
-    Log.i("boolean   = " + RandomUtil.getBoolean());
-    Log.i("String    = " + RandomUtil.getString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 8));
-    Log.i("String    = " + RandomUtil.getString("あいうえお", 8));
+    Log.i("int       = " + RandomGenerator.getInt());
+    Log.i("int 10    = " + RandomGenerator.getInt(10));
+    Log.i("int 1-6   = " + RandomGenerator.getInt(1, 6));
+    Log.i("int 10-20 = " + RandomGenerator.getInt(10, 20));
+
+    Log.i("long      = " + RandomGenerator.getLong());
+    Log.i("float     = " + RandomGenerator.getFloat());
+    Log.i("double    = " + RandomGenerator.getDouble());
+    Log.i("boolean   = " + RandomGenerator.getBoolean());
+    Log.i("char      = " + RandomGenerator.getChar());
+    char ch = RandomGenerator.getChar(0x21, 0x7E);
+    Log.i("char      = " + ch + " (" + (int) (ch) + ")");
+
+    String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    Log.i("String    = " + RandomGenerator.getString(chars, 8));
+    Log.i("String    = " + RandomGenerator.getString("あいうえお", 8));
   }
 
 }
