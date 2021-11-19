02209c5d1b937c25393d380304233879d6f02885
Takashi Harano <takashiharano@users.noreply.github.com>
2021-11-20 02:00:32 +0900

    Add test code

---
+++ python/test/file/test_rename.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+import util
+
+def test():
+  util.rename('a.txt', 'a_.txt')
+  return ret
+
+def main():
+  try:
+    ret = test()
+  except Exception as e:
+    ret = str(e)
+  print('OK')
+
+main()
