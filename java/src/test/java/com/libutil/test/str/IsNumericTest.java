b8c6220c64147cbfdee37b372175461f7c87a037
Takashi Harano <takashiharano@users.noreply.github.com>
2021-12-06 00:20:46 +0900

    Add IsAsciiTest.java

---
+++ java/src/test/java/com/libutil/test/str/IsNumericTest.java
@@ -0,0 +1,167 @@
+package com.libutil.test.str;
+
+import com.libutil.StrUtil;
+import com.libutil.test.Log;
+
+public class IsNumericTest {
+
+  public static void main(String args[]) {
+    isIntegerTest();
+    isFloatTest();
+    isNumericTest();
+    isNaNTest();
+    isNumberTest();
+  }
+
+  private static void isIntegerTest() {
+    String str = "1";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "-1";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "1234";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "1.2";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "-1.2";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "1.";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+
+    str = "-1.";
+    Log.d(str + "=" + StrUtil.isInteger(str));
+  }
+
+  private static void isFloatTest() {
+    String str = "1";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "-1";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "1234";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "1.2";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "-1.2";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "1.";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+
+    str = "-1.";
+    Log.d(str + "=" + StrUtil.isFloat(str));
+  }
+
+  private static void isNumericTest() {
+    Log.d("isNumberTest");
+
+    String str = "1";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "-1";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "1234";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "1.2";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "-1.2";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "1.";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "-1.";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "abc";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = "";
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+
+    str = null;
+    Log.d(str + "=" + StrUtil.isNumeric(str));
+  }
+
+  private static void isNaNTest() {
+    Log.d("isNaNTest");
+
+    String str = "1";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "-1";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "1234";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "1.2";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "-1.2";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "1.";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "-1.";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "abc";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = "";
+    Log.d(str + "=" + StrUtil.isNaN(str));
+
+    str = null;
+    Log.d(str + "=" + StrUtil.isNaN(str));
+  }
+
+  private static void isNumberTest() {
+    Log.d("isNumberTest");
+
+    String str = "1";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "-1";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "1234";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "1.2";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "-1.2";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "1.";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "-1.";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "abc";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = null;
+    Log.d(str + "=" + StrUtil.isNumber(str));
+
+    str = "１";
+    Log.d(str + "=" + StrUtil.isNumber(str));
+  }
+
+}
--- java/src/test/java/com/libutil/test/str/StrUtilTest.java
+++ java/src/test/java/com/libutil/test/str/StrUtilTest.java
@@ -9,10 +9,6 @@ public class StrUtilTest {
   public static void main(String args[]) {
     text2arrayTest();
     countStrPatternTest();
-    isIntegerTest();
-    isFloatTest();
-    isNumberTest();
-    isNaNTest();
     quoteTest();
     intnum2decimalTest();
     trimZerosTest();
@@ -33,120 +29,6 @@ public class StrUtilTest {
     String str = "abc123abc456xyz";
     Log.d("bc=" + StrUtil.countStrPattern(str, "bc"));
     Log.d("\\d\\d=" + StrUtil.countStrPattern(str, "\\d\\d"));
-  }
-
-  private static void isIntegerTest() {
-    String str = "1";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "-1";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "1234";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "1.2";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "-1.2";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "1.";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-
-    str = "-1.";
-    Log.d(str + "=" + StrUtil.isInteger(str));
-  }
-
-  private static void isFloatTest() {
-    String str = "1";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "-1";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "1234";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "1.2";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "-1.2";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "1.";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-
-    str = "-1.";
-    Log.d(str + "=" + StrUtil.isFloat(str));
-  }
-
-  private static void isNumberTest() {
-    Log.d("isNumberTest");
-
-    String str = "1";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "-1";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "1234";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "1.2";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "-1.2";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "1.";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "-1.";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "abc";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = "";
-    Log.d(str + "=" + StrUtil.isNumber(str));
-
-    str = null;
-    Log.d(str + "=" + StrUtil.isNumber(str));
-  }
-
-  private static void isNaNTest() {
-    Log.d("isNaNTest");
-
-    String str = "1";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "-1";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "1234";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "1.2";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "-1.2";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "1.";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "-1.";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "abc";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = "";
-    Log.d(str + "=" + StrUtil.isNaN(str));
-
-    str = null;
-    Log.d(str + "=" + StrUtil.isNaN(str));
   }
 
   private static void quoteTest() {
