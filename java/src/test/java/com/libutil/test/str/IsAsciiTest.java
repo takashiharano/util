1b612b89a3ebc7f78e4021b200e718cca916259e
Takashi Harano <takashiharano@users.noreply.github.com>
2021-12-07 00:05:24 +0900

    Update IsAsciiTest.java

--- java/src/test/java/com/libutil/test/str/IsAsciiTest.java
+++ java/src/test/java/com/libutil/test/str/IsAsciiTest.java
@@ -10,6 +10,7 @@ public class IsAsciiTest {
     Log.i("--- ASCII ---");
     TestUtil.assertTrue(StrUtil
         .isAscii(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"));
+    TestUtil.assertTrue(StrUtil.isAscii("\t"));
     TestUtil.assertFalse(StrUtil.isAscii(""));
     TestUtil.assertFalse(StrUtil.isAscii(null));
     TestUtil.assertFalse(StrUtil.isAscii("あ"));
@@ -18,6 +19,7 @@ public class IsAsciiTest {
     Log.i("--- non ASCII ---");
     TestUtil.assertFalse(StrUtil.isNonAscii(
         " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"));
+    TestUtil.assertFalse(StrUtil.isNonAscii("\t"));
     TestUtil.assertFalse(StrUtil.isNonAscii(""));
     TestUtil.assertFalse(StrUtil.isNonAscii(null));
     TestUtil.assertTrue(StrUtil.isNonAscii("あ"));
