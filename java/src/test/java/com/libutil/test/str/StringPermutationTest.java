bf28576d259d4ec0c6758c159780188b99257cbb
Takashi Harano <takashiharano@users.noreply.github.com>
2022-06-15 22:53:49 +0900

    Add performance test result comment

--- java/src/test/java/com/libutil/test/str/StringPermutationTest.java
+++ java/src/test/java/com/libutil/test/str/StringPermutationTest.java
@@ -7,6 +7,21 @@ public class StringPermutationTest {
     test("abcdefghijklmnopqrstuvwxyz", "ffff");
   }
 
+  // Core i7-7700 3.6GHz
+  // lastIndex(total count) = 475254
+  // targetIndex = 109674
+  // 10000: ntp 391ms
+  // 20000: acof 1343ms
+  // 30000: ariv 2886ms
+  // 40000: bgdl 5107ms
+  // 50000: buyb 8125ms
+  // 60000: cjsr 11860ms
+  // 70000: cynh 16203ms
+  // 80000: dnhx 21321ms
+  // 90000: eccn 27126ms
+  // 100000: eqxd 33531ms
+  // 109674: ffff
+  // 40.524s
   public static void test(String chars, String target) {
     if (chars == null) {
       chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
@@ -26,7 +41,7 @@ public class StringPermutationTest {
         System.out.println(i + ": " + str);
         break;
       }
-      if (i % 1000 == 0) {
+      if (i % 10000 == 0) {
         elapsed = System.currentTimeMillis() - start;
         System.out.println(i + ": " + str + " " + elapsed + "ms");
       }
