1651478f349682bb398e78d38f55be48f8746e51
Takashi Harano <takashiharano@users.noreply.github.com>
2022-11-14 19:20:01 +0900

    Fix encodeBase64()

--- js/util.js
+++ js/util.js
@@ -5,7 +5,7 @@
  * https://libutil.com/
  */
 var util = util || {};
-util.v = '202210300048';
+util.v = '202211141917';
 
 util.SYSTEM_ZINDEX_BASE = 0x7ffffff0;
 util.DFLT_FADE_SPEED = 500;
@@ -6593,12 +6593,7 @@ util.Base64.decode = function(str) {
 
 util.encodeBase64 = function(s) {
   if (s == undefined) return '';
-  var r;
-  try {
-    r = btoa(s);
-  } catch (e) {
-    r = btoa(encodeURIComponent(s).replace(/%([0-9A-F]{2})/g, function(match, p1) {return String.fromCharCode('0x' + p1);}));
-  }
+  var r = btoa(encodeURIComponent(s).replace(/%([0-9A-F]{2})/g, function(match, p1) {return String.fromCharCode('0x' + p1);}));
   return r;
 };
 util.decodeBase64 = function(s) {
