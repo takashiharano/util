d863a94b53dc210d77f3e9cb5d4ec6a833a720c3
Takashi Harano <takashiharano@users.noreply.github.com>
2023-06-17 19:35:08 +0900

    Add split_keywords test

---
+++ python/test/string/split_keywords_test.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+import util
+
+TEST_DATA_IN = [
+    'aaa',
+    'aaa bbb',
+    'aaa bbb ccc',
+    '"aaa bbb"',
+    'aaa "bbb ccc"',
+    'aaa "bbb ccc" ddd',
+    '"aaa\"bbb\""',
+    '"aaa\"bbb\"" ccc',
+    'aaa:"bbb ccc"',
+    'aaa bbb:"ccc ddd"',
+    'aaa bbb:"ccc ddd" eee',
+    '\"aaa\"'
+]
+
+TEST_DATA_IN2 = [
+    'aaa',
+    'aaa bbb',
+    'aaa bbb ccc',
+    '\'aaa bbb\'',
+    'aaa \'bbb ccc\'',
+    'aaa \'bbb ccc\' ddd',
+    '\'aaa\'bbb\'\'',
+    '\'aaa\'bbb\'\' ccc',
+    'aaa:\'bbb ccc\'',
+    'aaa bbb:\'ccc ddd\'',
+    'aaa bbb:\'ccc ddd\' eee',
+    '\'aaa\''
+]
+
+
+TEST_DATA_IN3 = [
+    '(aaa)',
+    '(aaa bbb)',
+    'aaa (bbb ccc)',
+    '(aaa bbb) ccc',
+    'aaa (bb(b) ccc)'
+]
+
+def test1(data_in):
+    print('----------')
+    print(data_in)
+    word_list = util.split_keywords(data_in)
+    for i in range(len(word_list)):
+        word = word_list[i]
+        print('[' + str(i) + '] ' + word)
+    print('\n')
+
+def main():
+    for i in range(len(TEST_DATA_IN)):
+        data_in = TEST_DATA_IN[i]
+        test1(data_in)
+
+    for i in range(len(TEST_DATA_IN2)):
+        data_in = TEST_DATA_IN2[i]
+        test1(data_in)
+
+    for i in range(len(TEST_DATA_IN3)):
+        data_in = TEST_DATA_IN3[i]
+        test1(data_in)
+
+main()
